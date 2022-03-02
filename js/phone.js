882d58c1a82ee872669d35e1cb690d7142f4d52c
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-02 12:19:23 +0600

    dynamic info added

--- js/phone.js
+++ js/phone.js
@@ -13,7 +13,20 @@ const searchButton =()=>{
 //display data ...........
 const displayData = (info) =>{
     info.forEach(phone=>{
-        console.log(phone);
+        const div = document.createElement("div");
+        div.classList.add("col-lg-4","mb-4")
+        div.innerHTML=`
+        <div class="card mx-auto text-center" style="width: 18rem;">
+          <img src="${phone.image}" class="card-img-top" alt="...">
+           <div class="card-body">
+               <h5 class="card-title">${phone.phone_name}</h5>
+               <p class="card-text">${phone.brand}</p>
+               <div class="buttons d-flex justify-content-around">
+               <a href="#" class="btn btn-success" onclick="Details('${phone.slug}')">Details</a>
+          </div>
+        </div>
+        </div>`
+        displayBoxData.appendChild(div);
     })
 };
 
