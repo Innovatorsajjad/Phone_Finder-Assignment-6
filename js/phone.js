60c2d10006f10f27519d041bb1da69d0344afa3a
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-02 12:53:12 +0600

    error added

--- js/phone.js
+++ js/phone.js
@@ -1,22 +1,37 @@
 //variable diclaration.............
  const displayBoxData = document.getElementById("DisplayBoxOfData");
+ const error = document.getElementById("error");
 
 // Connect search Button
 const searchButton =()=>{
     const searchBox = document.getElementById("SearchBox");
     const searchBoxValue= searchBox.value ;
+    if(searchBoxValue==""){
+        displayBoxData.innerHTML=""
+        error.innerHTML=`
+        <h3> Please inter a Phone Name.</h3>
+        `
+        return
+    }
+  else{
+    error.innerHTML="";
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchBoxValue}`
     fetch(url)
     .then(response=>response.json())
     .then(data=>displayData(data.data));
+    searchBox.value ="";
+  }
 };
 //display data ...........
+
 const displayData = (info) =>{
-    info.forEach(phone=>{
+    const firstTwenty = info.slice(0,20)
+    
+    firstTwenty.forEach(phone=>{
         const div = document.createElement("div");
         div.classList.add("col-lg-4","mb-4")
         div.innerHTML=`
-        <div class="card mx-auto text-center" style="width: 18rem;">
+        <div class="mx-auto text-center" style="width: 18rem;">
           <img src="${phone.image}" class="card-img-top" alt="...">
            <div class="card-body">
                <h5 class="card-title">${phone.phone_name}</h5>
